c71c03ee9393363434580b20525666d06de1eafa
Nedgs <nadergwasmi25@gmail.com>
2022-03-11 16:20:24 +0100

    dev

--- src/main/java/io/getarrays/userservice/UserserviceApplication.java
+++ src/main/java/io/getarrays/userservice/UserserviceApplication.java
@@ -1,13 +1,44 @@
 package io.getarrays.userservice;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.getarrays.userservice.domain.Role;
+import io.getarrays.userservice.domain.User;
+import io.getarrays.userservice.service.UserService;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Jhon Travolta", "jhon", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Tned Tneddd", "tned", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("jhon", "ROLE_USER");
+			userService.addRoleToUser("jhon", "ROLE_MANAGER");
+			userService.addRoleToUser("Will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("tned", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("tned", "ROLE_ADMIN");
+			userService.addRoleToUser("tned", "ROLE_USER");
+		};
 	}
 
 }
